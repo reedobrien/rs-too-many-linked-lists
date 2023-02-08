2635ed024cc6590e2713f6e0327b60d0482fe838
Reed O'Brien <reed@reedobrien.com>
2023-02-07 21:31:02 -1000

    chore: use raw pointer in fifth
    UB but seems to test.
    Miri fails.

--- src/fifth.rs
+++ src/fifth.rs
@@ -1,8 +1,8 @@
-use std::mem;
+use std::{mem, ptr};
 
-pub struct List<'a, T> {
+pub struct List<T> {
     head: Link<T>,
-    tail: Link<Option<&'a mut T>>,
+    tail: *mut Node<T>,
 }
 
 type Link<T> = Option<Box<Node<T>>>;
@@ -13,29 +13,41 @@ pub struct Node<T> {
     next: Link<T>,
 }
 
-impl<'a, T> List<'a, T> {
+impl<T> List<T> {
     pub fn new() -> Self {
         List {
             head: None,
-            tail: None,
+            tail: ptr::null_mut(),
         }
     }
 
-    pub fn push(&'a mut self, elem: T) {
-        let new_tail = Box::new(Node { elem, next: None });
+    pub fn pop(&mut self) -> Option<T> {
+        self.head.take().map(|head| {
+            let head = *head;
+            self.head = head.next;
 
-        let new_tail = match self.head.take() {
-            Some(old_tail) => {
-                old_tail.next = Some(new_tail);
-                old_tail.next.as_deref_mut()
+            if self.head.is_none() {
+                self.tail = ptr::null_mut();
             }
-            None => {
-                self.head = Some(new_tail);
-                self.head.as_deref_mut()
-            }
-        };
 
-        self.tail = new_tail;
+            head.elem
+        })
+    }
+
+    pub fn push(&mut self, elem: T) {
+        let mut new_tail = Box::new(Node { elem, next: None });
+
+        let raw_tail: *mut _ = &mut *new_tail;
+
+        if !self.tail.is_null() {
+            unsafe {
+                (*self.tail).next = Some(new_tail);
+            }
+        } else {
+            self.head = Some(new_tail);
+        }
+
+        self.tail = raw_tail;
     }
 }
 
@@ -48,5 +60,35 @@ mod test {
         let tut = List::<i32>::new();
 
         assert_eq!(tut.head, None)
+    }
+
+    #[test]
+    fn push_pop() {
+        let mut tut = List::<i32>::new();
+        assert_eq!(tut.pop(), None);
+
+        tut.push(1);
+        tut.push(2);
+        tut.push(3);
+        tut.push(4);
+
+        assert_eq!(tut.pop(), Some(1));
+        assert_eq!(tut.pop(), Some(2));
+
+        tut.push(5);
+        tut.push(6);
+
+        assert_eq!(tut.pop(), Some(3));
+        assert_eq!(tut.pop(), Some(4));
+        assert_eq!(tut.pop(), Some(5));
+        assert_eq!(tut.pop(), Some(6));
+        assert_eq!(tut.pop(), None);
+
+        tut.push(7);
+        tut.push(8);
+
+        assert_eq!(tut.pop(), Some(7));
+        assert_eq!(tut.pop(), Some(8));
+        assert_eq!(tut.pop(), None);
     }
 }
